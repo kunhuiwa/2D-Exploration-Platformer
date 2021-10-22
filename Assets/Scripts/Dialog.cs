039b34cf7eeec353727acc521eb643255f9ad379
Kelly <kelly.wang2002@hotmail.com>
2021-10-22 09:55:38 +0800

    Dialogues

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -3,7 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-/*
+    public class Dialog : MonoBehaviour
+    {
+        public TextMeshProUGUI textDisplay;
+        public string[] sentences;
+        private int index;
+        public float typingSpeed;
+
+        public GameObject continueButton;
+
+        void Start()
+        {
+            StartCoroutine(Type());
+        }
+
+        void Update()
+        {
+            if (textDisplay.text == sentences[index])
+            {
+                continueButton.SetActive(true);
+            }
+        }
+
+        IEnumerator Type()
+        {
+            foreach (char letter in sentences[index].ToCharArray())
+            {
+                textDisplay.text += letter;
+                yield return new WaitForSeconds(0.02f);
+
+            }
+        }
+
+        public void NextSentence()
+        {
+            continueButton.SetActive(false);
+
+            if (index < sentences.Length - 1)
+            {
+                index++;
+                textDisplay.text = "";
+                StartCoroutine(Type());
+            }
+            else
+            {
+                textDisplay.text = "";
+                continueButton.SetActive(false);
+            }
+        }
+
+    /*
 public class Dialog : MonoBehaviour
 {
     public TextMeshProUGUI textDisplay;
@@ -81,53 +130,5 @@ public class Dialog : MonoBehaviour
 
 */
 
-    public class Dialog : MonoBehaviour
-    {
-        public TextMeshProUGUI textDisplay;
-        public string[] sentences;
-        private int index;
-        public float typingSpeed;
 
-        public GameObject continueButton;
-
-        void Start()
-        {
-            StartCoroutine(Type());
-        }
-
-        void Update()
-        {
-            if (textDisplay.text == sentences[index])
-            {
-                continueButton.SetActive(true);
-            }
-        }
-
-        IEnumerator Type()
-        {
-            foreach (char letter in sentences[index].ToCharArray())
-            {
-                textDisplay.text += letter;
-                yield return new WaitForSeconds(0.02f);
-
-            }
-        }
-
-        public void NextSentence()
-        {
-            continueButton.SetActive(false);
-
-            if (index < sentences.Length - 1)
-            {
-                index++;
-                textDisplay.text = "";
-                StartCoroutine(Type());
-            }
-            else
-            {
-                textDisplay.text = "";
-                continueButton.SetActive(false);
-            }
-        }
-
-    }
+}
